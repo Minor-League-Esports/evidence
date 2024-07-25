69868ba0aa692b3734eabd3d9164d0a51aa5c808
Mark Crossland <crosslandmb@gmail.com>
2024-07-25 18:04:01 -0400

    Adding logo, github repo and X profile

---
+++ pages/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} title="Minor League Esports" logo=/MLE4.png githubRepo=https://github.com/Minor-League-Esports/evidence xProfile=https://twitter.com/MLEsportsGG >
+	<slot slot="content" />
+</EvidenceDefaultLayout>
