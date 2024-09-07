2e3cf60ccaa2707bc46754811a8f61e91ea3dcdf
Mark Crossland <crosslandmb@gmail.com>
2024-09-07 15:46:16 -0400

    renamed from S17 player stats to just S17 stats. Took team name off datatable. Made each row clickable to take to player page. Gave Player Comparison default values. Added League Comparison Tab to accomodate removable of that bar graph from main page in future PR. Also added Svelte input to make evidence full screen.

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -5,6 +5,6 @@
 	export let data;
 </script>
 
-<EvidenceDefaultLayout {data} logo=/mle_banner_logo.png githubRepo=https://github.com/Minor-League-Esports/evidence xProfile=https://twitter.com/MLEsportsGG >
+<EvidenceDefaultLayout {data} logo=/mle_banner_logo.png githubRepo=https://github.com/Minor-League-Esports/evidence xProfile=https://twitter.com/MLEsportsGG fullWidth=true >
 	<slot slot="content" />
 </EvidenceDefaultLayout>
