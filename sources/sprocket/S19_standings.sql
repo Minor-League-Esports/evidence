e0496d5d5afa5873f7b58eaab811c081ac05ef55
billybob <mattpokemore@gmail.com>
2026-02-02 16:24:44 -0600

    new datasets added

---
+++ sources/sprocket/S19_standings.sql
@@ -0,0 +1,3 @@
+SELECT * FROM read_parquet(
+    'https://sprocket-public-datasets.nyc3.cdn.digitaloceanspaces.com/datasets/standings_s19.parquet'
+    )
---
+++ sources/sprocket/s19_rounds.sql
@@ -0,0 +1,3 @@
+ SELECT * FROM read_parquet(
+        'https://sprocket-public-datasets.nyc3.cdn.digitaloceanspaces.com/datasets/rounds_s19.parquet'
+    )
